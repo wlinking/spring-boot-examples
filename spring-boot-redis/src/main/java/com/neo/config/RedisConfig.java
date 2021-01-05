3c8f4a9e31ed4013af29ede398a9127f0ef7d989
jinwenlin <jinwenlin44@163.com>
2021-01-05 16:24:30 +0800

    update

--- spring-boot-redis/src/main/java/com/neo/config/RedisConfig.java
+++ spring-boot-redis/src/main/java/com/neo/config/RedisConfig.java
@@ -1,23 +1,20 @@
 package com.neo.config;
 
-import java.lang.reflect.Method;
-
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.core.RedisTemplate;
+
+import java.lang.reflect.Method;
 
 
 @Configuration
 @EnableCaching
-public class RedisConfig extends CachingConfigurerSupport{
-	
-	@Bean
-	public KeyGenerator keyGenerator() {
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public KeyGenerator keyGenerator() {
         return new KeyGenerator() {
             @Override
             public Object generate(Object target, Method method, Object... params) {
